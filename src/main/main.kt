f3d324aa23cb86ad05e7834c1885f598820bda2d
Ekaterina Efremova <katerina.usatykh@gmail.com>
2022-09-11 16:36:54 +0300

    add main

--- src/main/main.kt
+++ src/main/main.kt
@@ -1,5 +1,23 @@
 package ru.netology
 
 fun main() {
-    
+        val amount = 1000000
+        println("Комиссия составит " + result)
+    }
+
+    fun test(paymentSystem: String, amontMonth: double, amount: double) = when(paymentSystem){
+        if (paymentSystem == "vkPay")
+            vpP(amontMonth, amount) rerurn 0
+            if (paymentSystem == "Visa", "Mir")
+        visaPay(amontMonth, amount)
+            if (paymentSystem == "Master")
+        masterPay (amontMonth, amount)
+    }
+
+    fun visaPay (amount): double {
+        val commission = (amount / 100) * 0.75
+        val minCommission = 3500.0
+        val result = if (commission < minCommission) minCommission else commission
+        return result
+    }
 }
